bbd90726cd0c38ee7add53aa6caebf692c2b21fa
samade747 <samad.e747@gmail.com>
2024-02-18 10:10:27 +0500

    Update App.js

--- counterapp/src/App.js
+++ counterapp/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
-import { useState } from "react"
+import { useState } from "react";
 
 function App() {
 
