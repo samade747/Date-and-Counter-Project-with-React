fda8b3d12d309ecc42750cea2ff0ba38571e821d
samade747 <samad.e747@gmail.com>
2024-02-18 14:47:59 +0500

    247

--- counterapp/src/components/Statusbar.js
+++ counterapp/src/components/Statusbar.js
@@ -1,7 +1,8 @@
 export default function Statusbar({count}){
     const date = new Date()
     const convertToString = date.toDateString()
-
+    console.log(date)
+    console.log(convertToString)
     return(
         <h2 style={{fontSize: '20px', fontWeight: "500px", color: "white", backgroundColor: "black"}}>
             {count === 0 ? `Today ` + convertToString: `${count} Days From Today Is` + convertToString}
