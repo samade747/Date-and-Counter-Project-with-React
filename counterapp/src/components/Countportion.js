9f4ab221fd21e1f70a0e2e32738fc5f050d355fc
samade747 <samad.e747@gmail.com>
2024-02-19 11:43:00 +0500

    1142pm 19 2 2024

--- counterapp/src/components/Countportion.js
+++ counterapp/src/components/Countportion.js
@@ -9,10 +9,10 @@ export default function Countportion({setCount, count, step}){
     }
 
     return(
-        <div className="Counterbutton" style={{display: "flex", margin: "30px", padding: "10px"}}>
-            <button onClick={countingadd} style={{width: "100px", height: "100px", fontSize: "35px"}}>+</button>
-            <p style={{fontWeight: "1800px", fontSize: "35px", margin: "15px"}}>COUNT: {count}</p>
+        <div className="Counterbutton" style={{display: "flex", margin: "30px", padding: "10px"}}>           
             <button onClick={countingless} style={{width: "100px", height: "100px", fontSize: "35px"}}>-</button>
+            <p style={{fontWeight: "1800px", fontSize: "35px", margin: "15px"}}>COUNT: {count}</p>
+            <button onClick={countingadd} style={{width: "100px", height: "100px", fontSize: "35px"}}>+</button>
         </div>
 
     )
--- counterapp/src/components/Statusbar.js
+++ counterapp/src/components/Statusbar.js
@@ -1,11 +1,22 @@
-export default function Statusbar({count}){
-    const date = new Date()
-    const convertToString = date.toDateString()
-    console.log(date)
-    console.log(convertToString)
-    return(
-        <h2 style={{fontSize: '20px', fontWeight: "500px", color: "white", backgroundColor: "black"}}>
-            {count === 0 ? `Today ` + convertToString: `${count} Days From Today Is` + convertToString}
+export default function Statusbar({ count }) {
+    // gettng today date
+    const today = new Date()
+
+    // copyng a today date avoid modifying the original date object
+    const futureDate = new Date(today.getTime());
+
+      // Calculate the future date based on the count
+      futureDate.setDate(futureDate.getDate() + count)
+
+       // Convert the future date to a string
+       const dateString = futureDate.toDateString();
+
+        return(
+        <h2 style={{fontSize: '25px', fontWeight: "500px", color: "white", backgroundColor: "blue", borderRadius: '2px', paddin: '10px', border: '2px solid White'}}>
+            { count === 0
+            ? `Today ${today.toDateString()}` // Display today's date
+            : `${count} Days From Today Is  ${dateString}`
+            }
         </h2>
     )
 
